d2617dc8083ba8dcf2b05f313d5d58699cf4011b
Dardell <Maxgodest@gmail.com>
2022-08-20 19:41:08 +0300

    Add project files.

---
+++ DotNetCoreTestProject/Data/AppDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreTestProject.Data
+{
+    internal sealed class AppDBContext : DbContext
+    {
+        public DbSet<Post>? Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => 
+            dbContextOptionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=DotNetLocalProject;User ID=BackendUser;Password=12345;Timeout=1024;CommandTimeout=1024;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Post[] postsToSeed = new Post[6];
+
+            for (int i = 1; i <= 6; i++)
+            {
+                postsToSeed[i - 1] = new Post
+                {
+                    PostId = i,
+                    Title = $"Post {i}",
+                    Content = $"This is post {i} and that's it."
+                };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+        }
+    }
+}
---
+++ DotNetCoreTestProject/Data/Migrations/20220820105540_FirstMigration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DotNetCoreTestProject.Data.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "character varying(100000)", maxLength: 100000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "PostId", "Content", "Title" },
+                values: new object[,]
+                {
+                    { 1, "This is post 1 and that's it.", "Post 1" },
+                    { 2, "This is post 2 and that's it.", "Post 2" },
+                    { 3, "This is post 3 and that's it.", "Post 3" },
+                    { 4, "This is post 4 and that's it.", "Post 4" },
+                    { 5, "This is post 5 and that's it.", "Post 5" },
+                    { 6, "This is post 6 and that's it.", "Post 6" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
